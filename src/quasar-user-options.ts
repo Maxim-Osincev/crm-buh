b64580794b832198bbe3dd47439c438772b80d7b
osintsev_maksim <DZw~Gi0tB3{ekEVq>
2023-02-27 20:11:38 +0300

    init

---
+++ src/quasar-user-options.ts
@@ -0,0 +1,10 @@
+import './styles/quasar.scss'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+// To be used on app.use(Quasar, { ... })
+export const options: object = {
+    config: {},
+    plugins: {
+    }
+}
