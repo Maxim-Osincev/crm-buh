419f7b4f0897db648c4fffe30032fd6a9fb3fb7f
osintsev_maksim <DZw~Gi0tB3{ekEVq>
2023-02-28 09:29:05 +0300

    Fix

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:Maxim-Osincev/crm-buh.git main:gh-pages
+ git push -f git@github.com:Maxim-Osincev/crm-buh.git master:gh-pages
 
 cd -
